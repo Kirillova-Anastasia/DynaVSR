2f6a2e1b5070417c154491963977e4883f4980a4
ansty <kirillovaap@yandex.ru>
2021-04-12 11:28:43 +0300

    script

--- DynaVSR.sh
+++ DynaVSR.sh
@@ -3,7 +3,7 @@ pip install numpy==1.17 torch==1.3.1 torchvision==0.4.2 tensorboard==1.14.0 pyya
 cd codes/models/archs/dcn
 python setup.py develop
 cd /workspace/DynaVSR
-apt install -y libgl1-mesa-glx
+apt-get update && apt install -y libgl1-mesa-glx
 apt-get install -y libgtk2.0-dev
 
 python3 codes/test_dynavsr.py -opt /codes/options/test/EDVR/EDVR_1bn.yml
