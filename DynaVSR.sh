081ecd2c58711e4ee92723941b8c22163eb35835
ansty <kirillovaap@yandex.ru>
2021-04-19 23:26:37 +0300

    scrupt

--- DynaVSR.sh
+++ DynaVSR.sh
@@ -5,13 +5,3 @@ python setup.py develop
 cd /workspace/DynaVSR
 apt-get update && apt install -y libgl1-mesa-glx
 apt-get install -y libgtk2.0-dev
-
-python3 codes/test_dynavsr.py -opt codes/options/test/EDVR/EDVR_2b.yml -save_dir /output/DynaVSR
-python3 codes/test_dynavsr.py -opt codes/options/test/EDVR/EDVR_2g.yml -save_dir /output/DynaVSR
-python3 codes/test_dynavsr.py -opt codes/options/test/EDVR/EDVR_3b.yml -save_dir /output/DynaVSR
-python3 codes/test_dynavsr.py -opt codes/options/test/EDVR/EDVR_3g.yml -save_dir /output/DynaVSR
-
-python3 codes/test_dynavsr_V.py -opt codes/options/test/EDVR/EDVR_2b_V.yml -save_dir /output/DynaVSR
-python3 codes/test_dynavsr_V.py -opt codes/options/test/EDVR/EDVR_2g_V.yml -save_dir /output/DynaVSR
-python3 codes/test_dynavsr_V.py -opt codes/options/test/EDVR/EDVR_3b_V.yml -save_dir /output/DynaVSR
-python3 codes/test_dynavsr_V.py -opt codes/options/test/EDVR/EDVR_3g_V.yml -save_dir /output/DynaVSR
